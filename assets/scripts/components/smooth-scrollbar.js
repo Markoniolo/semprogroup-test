5eb7290ecaa6d9833bf652a279aa0d504d440c7d
Марк Медведев <140736007+Markoniolo@users.noreply.github.com>
2024-12-05 13:08:21 +0300

    remove comment

--- assets/scripts/components/smooth-scrollbar.js
+++ assets/scripts/components/smooth-scrollbar.js
@@ -1,10 +1,3 @@
 import Scrollbar from 'smooth-scrollbar'
 
-const scrollbar = Scrollbar.init(document.querySelector('.smooth-scrollbar'))
-
-const header = document.querySelector('.header')
-
-// scrollbar.addListener( status => {
-//   const offset = status.offset
-//   header.style.top = offset.y + 'px'
-// })
+Scrollbar.init(document.querySelector('.smooth-scrollbar'))
